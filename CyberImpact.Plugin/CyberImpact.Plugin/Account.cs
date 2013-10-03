be58583abadd7b6b5c50d0498a097e724a6012f3
mpkg <mayank.gadodia87@gmail.com>
2013-10-03 20:23:03 +0530

    3/10

---
+++ CyberImpact.Plugin/CyberImpact.Plugin/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace CyberImpact.Plugin.Account
+{
+    public class Validations : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+        }
+    }
+
+    public class History : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+        }
+    }
+}
