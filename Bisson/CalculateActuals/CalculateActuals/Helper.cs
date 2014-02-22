bb7a24632b44e0b384c05268196e67df87130369
mpkg <mayank.gadodia87@gmail.com>
2014-02-22 17:34:20 +0530

    Bisson EXE first commit

---
+++ Bisson/CalculateActuals/CalculateActuals/Helper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xrm.Sdk;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Net;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Crm.Sdk.Messages;
+
+
+namespace CalculateActuals
+{
+    /// <summary>
+    /// Class for operations with CRM system
+    /// </summary>
+    public static class Helper
+    {
+        /// <summary>
+        /// Check SSL certificate handler
+        /// </summary>
+        private static bool AcceptAllCertificatePolicy(Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Creates client to CRM 2013 web service using configured URL
+        /// </summary>
+        public static IOrganizationService GetCrmConnection()
+        { return GetCrmConnection(null); }
+
+        /// <summary>
+        /// Creates client to CRM 2013 web service with specified URL
+        /// </summary>
+        public static IOrganizationService GetCrmConnection(Uri crmUrl)
+        {
+            if (crmUrl == null)
+                crmUrl = new Uri(Properties.Settings.Default.CrmUrl);
+
+            if (Properties.Settings.Default.UseWindowsAuthentication)
+            {
+                //to ignore certificates errors
+                ServicePointManager.ServerCertificateValidationCallback = AcceptAllCertificatePolicy;
+                IServiceManagement<IOrganizationService> orgServiceManagement =
+                   ServiceConfigurationFactory.CreateManagement<IOrganizationService>(crmUrl);
+                AuthenticationCredentials credentials = new AuthenticationCredentials();
+                credentials.ClientCredentials.Windows.ClientCredential = System.Net.CredentialCache.DefaultNetworkCredentials;
+                return new OrganizationServiceProxy(orgServiceManagement, credentials.ClientCredentials);
+            }
+            else
+            {
+                ClientCredentials Credentials = new ClientCredentials();
+                ClientCredentials deviceCredentials = new ClientCredentials();
+                Credentials.Windows.ClientCredential = System.Net.CredentialCache.DefaultNetworkCredentials;
+                deviceCredentials = Microsoft.Crm.Services.Utility.DeviceIdManager.LoadDeviceCredentials();
+
+                //to ignore certificates errors
+                ServicePointManager.ServerCertificateValidationCallback = AcceptAllCertificatePolicy;
+
+                Uri OrganizationUri = crmUrl;
+                Uri HomeRealmUri = null;
+                OrganizationServiceProxy _serviceProxy = new OrganizationServiceProxy(OrganizationUri, HomeRealmUri, Credentials,
+                    deviceCredentials);
+                _serviceProxy.ClientCredentials.UserName.UserName = Properties.Settings.Default.UserName;
+                _serviceProxy.ClientCredentials.UserName.Password = Properties.Settings.Default.UserPassword;
+                _serviceProxy.ServiceConfiguration.CurrentServiceEndpoint.Behaviors.Add(new ProxyTypesBehavior());
+                return (IOrganizationService)_serviceProxy;
+            }
+
+        }
+
+        /// <summary>
+        /// Creates client to CRM 2013 web service with specified credentials using configured URL
+        /// </summary>
+        public static IOrganizationService GetCrmConnection(string username, string password)
+        { return GetCrmConnection(username, password, null); }
+
+        /// <summary>
+        /// Creates client to CRM 2013 web service with specified credentials with specified URL
+        /// </summary>
+        public static IOrganizationService GetCrmConnection(string username, string password, Uri crmUrl)
+        {
+            if (crmUrl == null)
+                crmUrl = new Uri(Properties.Settings.Default.CrmUrl);
+
+            if (Properties.Settings.Default.UseWindowsAuthentication)
+            {
+                //to ignore certificates errors
+                ServicePointManager.ServerCertificateValidationCallback = AcceptAllCertificatePolicy;
+                IServiceManagement<IOrganizationService> orgServiceManagement =
+                   ServiceConfigurationFactory.CreateManagement<IOrganizationService>(crmUrl);
+                AuthenticationCredentials credentials = new AuthenticationCredentials();
+                credentials.ClientCredentials.Windows.ClientCredential = new System.Net.NetworkCredential(username, password);
+                return new OrganizationServiceProxy(orgServiceManagement, credentials.ClientCredentials);
+            }
+            else
+            {
+                ClientCredentials Credentials = new ClientCredentials();
+                ClientCredentials deviceCredentials = new ClientCredentials();
+                Credentials.Windows.ClientCredential = System.Net.CredentialCache.DefaultNetworkCredentials;
+                deviceCredentials = Microsoft.Crm.Services.Utility.DeviceIdManager.LoadDeviceCredentials();
+
+                //to ignore certificates errors
+                ServicePointManager.ServerCertificateValidationCallback = AcceptAllCertificatePolicy;
+
+                Uri OrganizationUri = crmUrl;
+                Uri HomeRealmUri = null;
+                OrganizationServiceProxy _serviceProxy = new OrganizationServiceProxy(OrganizationUri, HomeRealmUri, Credentials,
+                    deviceCredentials);
+                _serviceProxy.ClientCredentials.UserName.UserName = username;
+                _serviceProxy.ClientCredentials.UserName.Password = password;
+                _serviceProxy.ServiceConfiguration.CurrentServiceEndpoint.Behaviors.Add(new ProxyTypesBehavior());
+                return (IOrganizationService)_serviceProxy;
+            }
+        }
+    }
+}
---
+++ Bisson/CalculateActuals/CalculateActuals/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace CalculateActuals
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IOrganizationService organizationService = Helper.GetCrmConnection();
+
+            #region Retrieve active Budget records
+
+            QueryExpression budgetQuery = new QueryExpression()
+            {
+                EntityName = "new_budget",
+                ColumnSet = new ColumnSet(new string[] { "new_objectif", "new_vendeur", "new_typedetravaux", "new_periodefiscale", "new_excercice" }),
+                Criteria = new FilterExpression()
+                {
+                    Conditions =
+                    {
+                        new ConditionExpression("statecode", ConditionOperator.Equal, 0)
+                    }
+                }
+            };
+            EntityCollection budgetRecords = organizationService.RetrieveMultiple(budgetQuery);
+
+            #endregion Retrieve active Budget records
+
+            decimal objectif;
+            EntityReference vendeur;
+            EntityReference typedetravaux;
+            int fiscalperiod;
+            int exercice;
+
+            for (int i = 0; i < budgetRecords.Entities.Count; i++)
+            {
+                objectif = budgetRecords.Entities[i].GetAttributeValue<decimal>("new_objectif");
+                vendeur = budgetRecords.Entities[i].GetAttributeValue<EntityReference>("new_vendeur");
+                typedetravaux = budgetRecords.Entities[i].GetAttributeValue<EntityReference>("new_typedetravaux");
+                fiscalperiod = budgetRecords.Entities[i].GetAttributeValue<OptionSetValue>("new_periodefiscale").Value;
+                exercice = budgetRecords.Entities[i].GetAttributeValue<OptionSetValue>("new_excercice").Value;
+
+                QueryExpression contractQuery1 = new QueryExpression()
+                {
+                    EntityName = "new_contracttravaux",
+                    ColumnSet = new ColumnSet("new_ventetotal"),
+                    Criteria = new FilterExpression()
+                   {
+                       Conditions =
+                    {
+                        new ConditionExpression("new_vendeur", ConditionOperator.Equal, vendeur),
+                        new ConditionExpression("new_typedetravaux", ConditionOperator.Equal, typedetravaux),
+                        new ConditionExpression("new_datecontrat", ConditionOperator.InFiscalPeriod, fiscalperiod),
+                        new ConditionExpression("new_datecontrat", ConditionOperator.InFiscalYear, exercice)
+                    }
+                   }
+                };
+                EntityCollection contractRecords = organizationService.RetrieveMultiple(contractQuery1);
+                for (int j = 0; j < contractRecords.Entities.Count; j++)
+                { 
+                    Console.WriteLine(); 
+                }
+                
+            }
+
+            Console.Read();
+        }
+    }
+}
